385d7df54ecb6b785e626e8632d1e48b6ed5d142
exZako <121549472+exZako@users.noreply.github.com>
2023-01-03 22:43:36 +0600

    Add files via upload

---
+++ 30.12.22.cpp
@@ -0,0 +1,97 @@
+﻿#include <cmath>
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Ru-ru");
+
+ /*   std::cout << "Задание 14.1.1";
+    std::cout << std::endl;
+    double y;
+    double e;
+    double x;
+    std::cout << std::endl;
+    std::cout << "Введите y: ";
+    std::cin >> y;
+    std::cout << "Введите e: ";
+    std::cin >> e;
+    std::cout << "Введите x: ";
+    std::cin >> x;
+    long double result = abs(sqrt(pow(sin(y), 2) + 6.835 + pow(e, x)));
+    std::cout << "R = " << result;*/
+
+
+    //std::cout << "Задание 14.1.2";
+    //std::cout << std::endl;
+    //double e;
+    //double y;
+    //double h;
+    //std::cout << std::endl;
+    //std::cout << "Введите e: ";
+    //std::cin >> e;
+    //std::cout << "Введите y: ";
+    //std::cin >> y;
+    //std::cout << "Введите h: ";
+    //std::cin >> h;
+    //double result = (pow(e, y + 2.5) + 7.1 * pow(h, 3)) / (log(abs(y + 0.04 * h)));
+    //std::cout << "P = " << result;
+
+     /*std::cout << "Задание 14.1.3";
+     std::cout << std::endl;
+     double x;
+     std::cout << std::endl;
+     std::cout << "Введите x: ";
+     std::cin >> x;
+     double result = (sin(pow((pow(x, 2) + 4), 3)) + 4.3) / (pow(sin(pow(x, 4)),3));
+     std::cout << "R = " << result;*/
+
+
+     //std::cout << "Задание 14.2.1";
+     //std::cout << std::endl;
+     //double e = 285;
+     //double R;
+     //double r;
+     //std::cout << std::endl;
+     //std::cout << "Введите R: ";
+     //std::cin >> R;
+     //std::cout << "Введите r: ";
+     //std::cin >> r;
+     //long double result = (e) / (R + r);
+     //std::cout << "L = " << result;
+
+
+   /*  std::cout << "Задание 14.2.2";
+     std::cout << std::endl;
+     double b = 6;
+     double k = 3.4;
+     double t = pow(b, 2) + pow(k, 3);
+     std::cout << "Т = " << t;
+     double a = sqrt(b + t);
+     std::cout << std::endl;
+     std::cout << "A = " << a;
+     double y = pow(sin(pow(a, 2) + pow(b, 2)), 4);
+     std::cout << std::endl;
+     std::cout << "Y = " << y;*/
+
+    
+    /*std::cout << "Задание 14.2.3";
+    std::cout << std::endl;
+    double x;
+    double sum = 0;
+    std::cout << std::endl;
+    std::cout << "Введите x: ";
+    std::cin >> x;
+    double result = -cos(x) + ((cos(2 * x)) / 4) - ((cos(3 * x)) / 9);
+    std::cout << "Result = " << result;
+    while (fabs(result) >= 1e-5) {
+        sum += result;
+        result += x * ((cos(2 * x)) / 4) - ((cos(3 * x)) / 9);
+        std::cout << "Res = " << result << std::endl;
+    }*/
+
+
+
+}
